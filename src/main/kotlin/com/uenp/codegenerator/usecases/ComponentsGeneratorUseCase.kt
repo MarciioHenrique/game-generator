599b6d880eafe13e7a2345f053bdce6bb1c69d18
Marcio Henrique <dev.marcioh@gmail.com>
2024-09-07 19:56:09 -0300

    feat: useCase that generates menu component

---
+++ src/main/kotlin/com/uenp/codegenerator/usecases/ComponentsGeneratorUseCase.kt
@@ -0,0 +1,36 @@
+package com.uenp.codegenerator.usecases
+
+import com.uenp.codegenerator.components.visuals.MenuComponent
+import com.uenp.codegenerator.controllers.requests.ComponentsRequest
+import org.springframework.stereotype.Service
+import java.io.File
+import java.nio.file.Files
+import java.nio.file.Paths
+
+@Service
+class ComponentsGeneratorUseCase {
+    fun perform(components: ComponentsRequest, baseDir: File) {
+        val scriptsDir = File(baseDir, "scripts")
+        if (!scriptsDir.exists()) {
+            scriptsDir.mkdirs()
+        }
+
+        val scenesDir = File(baseDir, "scenes")
+        if (!scenesDir.exists()) {
+            scenesDir.mkdirs()
+        }
+
+        if (components.menu == true) {
+            val scriptContent = MenuComponent().generateScript()
+            val scriptName = "menu.gd"
+            val scriptPath = Paths.get(scriptsDir.toString(), scriptName)
+            Files.write(scriptPath, scriptContent.toByteArray())
+
+            val sceneContent = MenuComponent().generateScene()
+            val sceneName = "menu.tscn"
+            val scenePath = Paths.get(scenesDir.toString(), sceneName)
+            Files.write(scenePath, sceneContent.toByteArray())
+        }
+
+    }
+}
