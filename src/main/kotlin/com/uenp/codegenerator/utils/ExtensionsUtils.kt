6948658c876d02364ea6127d5658582c6bbc772c
Marcio Henrique <dev.marcioh@gmail.com>
2024-09-07 21:53:15 -0300

    feat: copying a file and base path

--- src/main/kotlin/com/uenp/codegenerator/utils/ExtensionsUtils.kt
+++ src/main/kotlin/com/uenp/codegenerator/utils/ExtensionsUtils.kt
@@ -1,5 +1,16 @@
 package com.uenp.codegenerator.utils
 
+import java.io.File
+import java.nio.file.Files
+import java.nio.file.StandardCopyOption
+
 fun normalize(value: String): String {
     return value.lowercase().replace(" ", "-")
 }
+
+fun copyFile(source: File, target: File) {
+    println("Copying file from ${source.absolutePath} to ${target.absolutePath}")
+    Files.copy(source.toPath(), target.toPath(), StandardCopyOption.REPLACE_EXISTING)
+}
+
+const val BASE_PATH = "src/main/kotlin/com/uenp/codegenerator"
